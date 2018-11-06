6a5a8509b6590a58e0165a1b3fe6b5c201a86309
Tara Craig <tara.craig7@gmail.com>
2018-11-06 16:35:03 -0700

    Add additional expectations.

--- spec/requests/api/v1/members_request_spec.rb
+++ spec/requests/api/v1/members_request_spec.rb
@@ -7,5 +7,13 @@ describe 'Members API' do
     get '/api/v1/members'
 
     expect(response.status).to eq(200)
+
+    members = JSON.parse(response.body)
+
+    expect(members.count).to eq(3)
+    expect(members.first).to have_key('name')
+    expect(members.first).to have_key('role')
+    expect(members.first).to have_key('party')
+    expect(members.first).to have_key('next_election')
   end
 end
