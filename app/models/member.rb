d4b65581a63afca171fb7bafc4062f0f036f267f
Tara Craig <tara.craig7@gmail.com>
2018-11-06 21:57:41 -0700

    Create member poro and spec

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,19 @@
 class Member
-  def initialize(chamber, state)
-    @chamber = chamber
-    @state = state
+  attr_reader     :name,
+                  :role,
+                  :party,
+                  :twitter,
+                  :facebook,
+                  :seniority,
+                  :next_election
+
+  def initialize(member_data)
+    @name = member_data[:name]
+    @role = member_data[:role]
+    @party = member_data[:party]
+    @twitter = member_data[:twitter_id]
+    @facebook = member_data[:facebook]
+    @seniority = member_data[:seniority]
+    @next_election = member_data[:next_election]
   end
 end
