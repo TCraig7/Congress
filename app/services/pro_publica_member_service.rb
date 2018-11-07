3e930bfb0e24d3d503eff02020c08fc2e1042b19
Tara Craig <tara.craig7@gmail.com>
2018-11-06 18:54:36 -0700

    Add propublica member service and spec

--- app/services/pro_publica_member_service.rb
+++ app/services/pro_publica_member_service.rb
@@ -1,3 +1,18 @@
 class ProPublicaMemberService
+  def house_member_data(chamber, state)
+    get_json("/congress/v1/members/#{chamber}/#{state}/current.json")
+  end
 
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.propublica.org') do |faraday|
+      faraday.headers["X-API-KEY"] = ENV["propublica_api_key"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
 end
--- spec/services/pro_publica_member_service_spec.rb
+++ spec/services/pro_publica_member_service_spec.rb
@@ -6,4 +6,21 @@ describe ProPublicaMemberService do
 
     expect(propublica).to be_a(ProPublicaMemberService)
   end
+
+  it 'returns #member_data' do
+    VCR.use_cassette('member_data') do
+      propublica = ProPublicaMemberService.new
+      chamber = 'house'
+      state = 'CO'
+
+      response = propublica.house_member_data(chamber, state)
+
+      expect(response).to be_a(Hash)
+      expect(response).to have_key(:results)
+      expect(response[:results].first).to have_key(:name)
+      expect(response[:results].first).to have_key(:role)
+      expect(response[:results].first).to have_key(:party)
+      expect(response[:results].first).to have_key(:next_election)
+    end
+  end
 end
