2120b9509b1c4387c50e448d1f987e3734d3736c
Tara Craig <tara.craig7@gmail.com>
2018-11-29 14:55:55 -0700

    Add config block to application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module Congress
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get]
+      end
+    end
   end
 end
